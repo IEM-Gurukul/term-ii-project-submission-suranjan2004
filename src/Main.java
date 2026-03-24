fb352102ae0afb222756831427b00384ce59437d
Suranjan Kuila <suranjankuila35@gmail.com>
2026-03-24 19:42:33 +0530

    Student class inherit from person class

--- src/Main.java
+++ src/Main.java
@@ -18,3 +18,29 @@ public class Main {
     public static void main(String[] args) {
   }
 }
+class Student extends Person {
+    private int totalClasses;
+    private int attendedClasses;
+    public Student(String id, String name) {
+        super(id, name);
+        this.totalClasses = 0;
+        this.attendedClasses = 0;
+    }
+    public void markAttendance(boolean isPresent) {
+        this.totalClasses++;
+        if (isPresent) {
+            this.attendedClasses++;
+        }
+    }
+    public double getAttendancePercentage() {
+        if (totalClasses == 0) return 0.0;
+        return ((double) attendedClasses / totalClasses) * 100;
+    }
+    @Override
+    public void displayInfo() {
+        System.out.println("ID: " + getId() + 
+                   " | Name: " + getName() + 
+                   " | Attendance: " + attendedClasses + "/" + totalClasses + 
+                   " (" + getAttendancePercentage() + "%)");
+    }
+}
